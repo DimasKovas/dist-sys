015e2c79dbe79704238ace9a9eaff471e61cead8
Dmitrii Kovalkov <dimas.kovas@gmail.com>
2020-03-12 20:56:09 +0100

    Add DB, fix everything

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM    golang:latest
-WORKDIR /usr/src/app/
+WORKDIR /go/src/github.com/DimasKovas/dist-sys/
 COPY    . .
+RUN go get -u
 RUN go build main.go
+RUN wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+RUN chmod 0777 wait-for-it.sh
 CMD  ["./main"]
--- main.go
+++ main.go
@@ -2,215 +2,119 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"github.com/DimasKovas/dist-sys/dbclient"
 )
 
-type Item dbclient.Item
+type Item = dbclient.Item
 
 var db dbclient.Client
 
-type ItemList struct {
-	Items []Item `json:"items"`
-}
-
-func ConvertIdsFromStrings(ids []string) ([]uint64, error) {
-	var result []uint64
-	for _, strid := range ids {
-		id, err := strconv.ParseUint(strid, 10, 64)
-		if err != nil {
-			return nil, err
-		}
-		result = append(result, id)
-	}
-	return result, nil
-}
-
-func ItemHandler(w http.ResponseWriter, r *http.Request) {
-	stdid := r.URL.Path[len("/item/"):]
-	var ids [1]uint64
-	id, err := strconv.ParseUint(stdid, 10, 64)
-	ids[0] = id
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	if r.Method == http.MethodGet {
-		// prefix is /items/, skip it
-
-		GlobalGuard.RLock()
-		defer GlobalGuard.RUnlock()
-		result := GlobalItems.FilterByIds(ids[:])
-		if len(result.Items) != 1 {
-			http.Error(w, "Item not found", http.StatusNotFound)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(result.Items[0])
-	} else if r.Method == http.MethodDelete {
-		GlobalGuard.Lock()
-		defer GlobalGuard.Unlock()
-		oldLen := len(GlobalItems.Items)
-		GlobalItems = GlobalItems.FilterBy(func(item Item) bool {
-			return item.ID != id
-		})
-		if len(GlobalItems.Items) == oldLen {
-			http.Error(w, "Item not found", http.StatusNotFound)
-			return
-		}
-	} else if r.Method == http.MethodPut {
-		var item Item
-		err := json.NewDecoder(r.Body).Decode(&item)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		if id != item.ID {
-			http.Error(w, "ID dismiss", http.StatusBadRequest)
-			return
-		}
-		GlobalGuard.Lock()
-		defer GlobalGuard.Unlock()
-		oldLen := len(GlobalItems.Items)
-		GlobalItems = GlobalItems.FilterBy(func(item Item) bool {
-			return item.ID != id
-		})
-		if len(GlobalItems.Items) == oldLen {
-			http.Error(w, "Item not found", http.StatusNotFound)
-			return
-		}
-		GlobalItems.Items = append(GlobalItems.Items, item)
-	} else {
-		http.Error(w, "Unsupported method", http.StatusBadRequest)
-	}
-}
-
-func ItemListHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		GlobalGuard.RLock()
-		defer GlobalGuard.RUnlock()
-		result := GlobalItems
-		param := r.URL.Query()
-		values, ok := param["title"]
-		if ok {
-			result = result.FilterByTitles(values)
-		}
-		values, ok = param["category"]
-		if ok {
-			result = result.FilterByCategories(values)
-		}
-		values, ok = param["id"]
-		if ok {
-			ids, err := ConvertIdsFromStrings(values)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-			result = result.FilterByIds(ids)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(result)
-	} else if r.Method == http.MethodPost {
-		GlobalGuard.Lock()
-		defer GlobalGuard.Unlock()
-		var item Item
-		err := json.NewDecoder(r.Body).Decode(&item)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		var ids [1]uint64
-		ids[0] = item.ID
-		if len(GlobalItems.FilterByIds(ids[:]).Items) != 0 {
-			http.Error(w, "Item with this ID already exists", http.StatusBadRequest)
-			return
-		}
-		GlobalItems.Items = append(GlobalItems.Items, item)
-	} else {
-		http.Error(w, "Unsupported method", http.StatusBadRequest)
-	}
-}
-
-func responseOK(w http.ResponseWriter, result interface{}) {
+func respondOK(w http.ResponseWriter, result interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(result)
 }
 
-func responseError(w http.ResponseWriter, err error, statusCode int) {
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+func respondWithError(w http.ResponseWriter, err error, statusCode int) {
 	w.WriteHeader(statusCode)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(err)
+	json.NewEncoder(w).Encode(errorResponse{fmt.Sprint(err)})
+}
+
+func parseUint(s string) (uint64, error) {
+	return strconv.ParseUint(s, 10, 64)
+}
+
+func parseInt(s string) (int64, error) {
+	return strconv.ParseInt(s, 10, 64)
 }
 
 func extractIndexFromUrl(url string, pref string) (uint64, error) {
-	return strconv.ParseUint(strings.TrimPrefix(url, pref), 10, 64)
+	return parseUint(strings.TrimPrefix(url, pref))
 }
+
+type getItemResponse = Item
 
 func getItemHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := extractIndexFromUrl(r.URL.Path, "/item/")
 	if err != nil {
-		responseError(w, err, http.StatusBadRequest)
+		respondWithError(w, err, http.StatusBadRequest)
 		return
 	}
 	item, err := db.GetItem(id)
 	if err != nil {
-		responseError(w, err, http.StatusInternalServerError)
+		respondWithError(w, err, http.StatusInternalServerError)
 		return
 	}
-	responseOK(w, item)
+	respondOK(w, item)
+}
+
+type postItemResponse struct {
+	ID uint64 `json:"id"`
 }
 
 func postItemHandler(w http.ResponseWriter, r *http.Request) {
 	var item Item
 	err := json.NewDecoder(r.Body).Decode(&item)
 	if err != nil {
-		responseError(w, err, http.StatusBadRequest)
+		respondWithError(w, err, http.StatusBadRequest)
 		return
 	}
 	id, err := db.NewItem(item)
 	if err != nil {
-		responseError(w, err, http.StatusInternalServerError)
+		respondWithError(w, err, http.StatusInternalServerError)
 		return
 	}
-	responseOK(w, id)
+	respondOK(w, postItemResponse{id})
 }
+
+type putItemResponse struct{}
 
 func putItemHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := extractIndexFromUrl(r.URL.Path, "/item/")
 	if err != nil {
-		responseError(w, err, http.StatusBadRequest)
+		respondWithError(w, err, http.StatusBadRequest)
 		return
 	}
 	var item Item
 	err = json.NewDecoder(r.Body).Decode(&item)
 	if err != nil {
-		responseError(w, err, http.StatusBadRequest)
+		respondWithError(w, err, http.StatusBadRequest)
 		return
 	}
 	item.ID = id
 	err = db.UpdateItem(item)
 	if err != nil {
-		responseError(w, err, http.StatusInternalServerError)
+		respondWithError(w, err, http.StatusInternalServerError)
 		return
 	}
-	responseOK(w, nil)
+	respondOK(w, putItemResponse{})
 }
+
+type deleteItemResponse struct{}
 
 func deleteItemHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := extractIndexFromUrl(r.URL.Path, "/item/")
 	if err != nil {
-		responseError(w, err, http.StatusBadRequest)
+		respondWithError(w, err, http.StatusBadRequest)
 		return
 	}
 	err = db.DeleteItem(id)
 	if err != nil {
-		responseError(w, err, http.StatusBadRequest)
+		respondWithError(w, err, http.StatusBadRequest)
 		return
 	}
-	responseOK(w, nil)
+	respondOK(w, deleteItemResponse{})
 }
 
 func generalItemHandler(w http.ResponseWriter, r *http.Request) {
@@ -229,8 +133,47 @@ func generalItemHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getItemsHandler(w http.ResponseWriter, r *http.Request) {
+func extractUintFromParams(params url.Values, name string) (uint64, error) {
+	if len(params[name]) != 1 {
+		return 0, fmt.Errorf("Only one parameter '%v' can be specified", name)
+	}
+	return parseUint(params[name][0])
+}
 
+type getItemsResponse = []Item
+
+func getItemsHandler(w http.ResponseWriter, r *http.Request) {
+	var options dbclient.GetItemListOptions
+	params := r.URL.Query()
+	if params["offset"] != nil {
+		offset, err := extractUintFromParams(params, "offset")
+		if err != nil {
+			respondWithError(w, err, http.StatusBadRequest)
+			return
+		}
+		options.Offset = offset
+	}
+	limit := uint64(1000)
+	if params["limit"] != nil {
+		sLimit, err := extractUintFromParams(params, "limit")
+		if err != nil {
+			respondWithError(w, err, http.StatusBadRequest)
+			return
+		}
+		if sLimit < limit {
+			limit = sLimit
+		}
+	}
+	options.Limit = limit
+	if params["category"] != nil {
+		options.Categories = params["category"]
+	}
+	items, err := db.GetItemList(options)
+	if err != nil {
+		respondWithError(w, err, http.StatusInternalServerError)
+		return
+	}
+	respondOK(w, items)
 }
 
 func generalItemsHandler(w http.ResponseWriter, r *http.Request) {
@@ -244,8 +187,14 @@ func generalItemsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	GlobalItems.Items = make([]Item, 0)
-	http.HandleFunc("/items", ItemListHandler)
+	var err error
+	db, err = dbclient.CreateDbClient()
+	if err != nil {
+		log.Panic(err)
+	}
+	http.HandleFunc("/items", generalItemsHandler)
 	http.HandleFunc("/item", generalItemHandler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/item/", generalItemHandler)
+	log.Println("HTTP Server started")
+	log.Panic(http.ListenAndServe(":8080", nil))
 }
